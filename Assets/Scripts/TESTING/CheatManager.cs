5f99126ec0b19b0e8a8656f7e28440abf44a1107
1boker1 <nil.ll.lopez@gmail.com>
2020-09-22 19:09:13 +0200

    Upgraded Unity Version

--- Assets/Scripts/TESTING/CheatManager.cs
+++ Assets/Scripts/TESTING/CheatManager.cs
@@ -1,40 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Assets.Scripts.Managers;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CheatManager : MonoBehaviour
 {
-    public bool SeeHelpers { get; private set; }
+	public bool SeeHelpers { get; private set; }
+
 	public static CheatManager Instance;
 
 	private void Awake()
 	{
 #if UNITY_EDITOR
 		Destroy(gameObject);
-		if(Instance==null)
-				Instance=this;
-			if(Instance!=this)
-				Destroy(gameObject);
+		if (Instance == null)
+			Instance = this;
+		if (Instance != this)
+			Destroy(gameObject);
 
 		Debug.LogError("Quitar los cheats para la build final");
 #endif
 
 	}
-	void Update()
-    {
+
+	private void Update()
+	{
 #if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.L))
-			GetComponent<Light>().enabled=!GetComponent<Light>().enabled;
-        //if (Input.GetKeyDown(KeyCode.C)) SeeHelpers = !SeeHelpers;
-        if (Input.GetKeyDown(KeyCode.RightControl)) Time.timeScale = 5;
-        if (Input.GetKeyUp(KeyCode.RightControl)) Time.timeScale = 1;
-        if (Input.GetKeyDown(KeyCode.RightShift)) Time.timeScale = 0.5f;
-        if (Input.GetKeyUp(KeyCode.RightShift)) Time.timeScale = 1;
-        //if (Input.GetKeyDown(KeyCode.Delete))
-        //    foreach (var unit in SelectionManager.SelectedUnits)
-        //        Destroy(unit.gameObject);
+			GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+		//if (Input.GetKeyDown(KeyCode.C)) SeeHelpers = !SeeHelpers;
+		if (Input.GetKeyDown(KeyCode.RightControl))
+			Time.timeScale = 5;
+		if (Input.GetKeyUp(KeyCode.RightControl))
+			Time.timeScale = 1;
+		if (Input.GetKeyDown(KeyCode.RightShift))
+			Time.timeScale = 0.5f;
+		if (Input.GetKeyUp(KeyCode.RightShift))
+			Time.timeScale = 1;
+		//if (Input.GetKeyDown(KeyCode.Delete))
+		//    foreach (var unit in SelectionManager.SelectedUnits)
+		//        Destroy(unit.gameObject);
 #endif
 
-    }
+	}
 }
